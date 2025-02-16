105c209bf0e5007c2b048c66ac136d010870eb8a
Rakhymzhan01 <Rakhymzhan0001@gmail.com>
2025-02-16 21:46:52 +0600

    done part 2

---
+++ lab4/part2/angular-products/src/app/app.component.ts
@@ -0,0 +1,18 @@
+// app.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './products/products.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProductsComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'online-store';
+}
