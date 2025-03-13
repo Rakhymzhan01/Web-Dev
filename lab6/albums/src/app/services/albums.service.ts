54af8088b9c6ea39654f030e50eb9a3c9ba03fdd
Rakhymzhan01 <Rakhymzhan0001@gmail.com>
2025-03-13 14:58:47 +0600

    lab6

---
+++ lab6/albums/src/app/services/albums.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AlbumELement } from '../model/album.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AlbumService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+}
